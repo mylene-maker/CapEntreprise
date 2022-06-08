20fbe0e30bcf19e19e0285cdcb63628a232efaf3
mylene-maker <71381585+mylene-maker@users.noreply.github.com>
2022-06-08 09:53:48 +0200

    first commit

---
+++ src/main/java/com/hb/capentreprise/CapentrepriseApplication.java
@@ -0,0 +1,13 @@
+package com.hb.capentreprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapentrepriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapentrepriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hb/capentreprise/CapentrepriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.capentreprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapentrepriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
