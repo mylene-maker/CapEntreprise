971edb5ae2360df34910ee804d12180db127ff1c
unknown <krariajordan@gmail.com>
2022-06-08 16:38:21 +0200

    modifcation classification

--- src/main/java/com/hb/capentreprise/entities/Classification.java
+++ src/main/java/com/hb/capentreprise/entities/Classification.java
@@ -26,56 +26,41 @@ public class Classification {
 	private List<Game> games = new ArrayList<Game>();
 
 	
-	
 	public Classification() {
 		super();
 	}
 
 
-
-	public Classification(Long id, String name, List<Game> reviews) {
+	public Classification(Long id, String name, List<Game> games) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.reviews = reviews;
+		this.games = games;
 	}
-
-
 
 	public Long getId() {
 		return id;
 	}
 
-
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-
 
 	public String getName() {
 		return name;
 	}
 
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
-
-	public List<Game> getReviews() {
-		return reviews;
+	public List<Game> getGames() {
+		return games;
 	}
 
-
-
-	public void setReviews(List<Game> reviews) {
-		this.reviews = reviews;
+	public void setGames(List<Game> games) {
+		this.games = games;
 	}
-	
-	
+
 	
 }
